0114ca82b18badef9b2ce165c46820fe5c0fb58c
youyouguo430 <139545589+youyouguo430@users.noreply.github.com>
2024-01-26 21:17:04 +0800

    config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,9 +18,9 @@ const CONFIG = {
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_4: '哈哈哈哈',
+  HEO_HERO_TITLE_5: '还没想好这里要放什么',
+  HEO_HERO_TITLE_LINK: '这里也没想好',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
