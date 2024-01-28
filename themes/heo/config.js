1f2c8bf49e690654f15b9b9d105911f308c0f4c7
youyouguo430 <139545589+youyouguo430@users.noreply.github.com>
2024-01-28 21:12:43 +0800

    config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -34,13 +34,7 @@ const CONFIG = {
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+    '你好！我们是',
   ],
   HEO_INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
 
@@ -104,11 +98,11 @@ const CONFIG = {
     }
   ],
 
-  HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD: false, // 是否显示右侧，点击加入社群按钮
+  //HEO_SOCIAL_CARD_TITLE_1: '交流频道',
+  //HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
+  //HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
+  //HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
