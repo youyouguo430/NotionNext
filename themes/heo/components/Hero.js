117192c1d0bcd895bea4d6002e4f01c425ad4cf3
youyouguo430 <139545589+youyouguo430@users.noreply.github.com>
2024-01-29 09:39:54 +0800

    Hero.js

--- themes/heo/components/Hero.js
+++ themes/heo/components/Hero.js
@@ -250,7 +250,7 @@ function TopGroup(props) {
                   {p?.title}
                 </div>
                 {/* hover 悬浮的 ‘荐’ 字 */}
-                <div className="opacity-0 group-hover:opacity-100 -translate-x-4 group-hover:translate-x-0 duration-200 transition-all absolute -top-2 -left-2 bg-indigo-600 dark:bg-yellow-600  text-white rounded-xl overflow-hidden pr-2 pb-2 pl-4 pt-4 text-xs">
+                <div className="opacity-0 group-hover:opacity-100 -translate-x-4 group-hover:translate-x-0 duration-200 transition-all absolute -top-2 -left-2 bg-[#b96a6f] dark:bg-yellow-600  text-white rounded-xl overflow-hidden pr-2 pb-2 pl-4 pt-4 text-xs">
                   荐
                 </div>
               </div>
