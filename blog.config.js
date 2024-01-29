1ebd9fefaeec1eeeca5c8eded114f25075502ff7
youyouguo430 <139545589+youyouguo430@users.noreply.github.com>
2024-01-29 15:45:32 +0800

    blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -167,10 +167,10 @@ const BLOG = {
   FIREWORKS: process.env.NEXT_PUBLIC_FIREWORKS || true, // 开关
   // 烟花色彩，感谢 https://github.com/Vixcity 提交的色彩
   FIREWORKS_COLOR: [
-    '255, 20, 97',
-    '24, 255, 146',
-    '90, 135, 255',
-    '251, 243, 140'
+    '90, 56, 34',
+    '173, 106, 107',
+    '233, 222, 203',
+    '233, 131, 143'
   ],
 
   // 樱花飘落特效
