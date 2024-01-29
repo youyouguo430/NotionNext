bf758dce913016efcfd3e95fc73d2541a934e93f
youyouguo430 <139545589+youyouguo430@users.noreply.github.com>
2024-01-29 14:56:31 +0800

    config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -51,10 +51,10 @@ const CONFIG = {
     {
       title_1: 'Docker',
       img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
-      color_1: '#57b6e6',
+      color_1: '#e9838f',
       title_2: 'Photoshop',
       img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
-      color_2: '#4082c3'
+      color_2: '#ad6a6b'
     },
     {
       title_1: 'FinalCutPro',
