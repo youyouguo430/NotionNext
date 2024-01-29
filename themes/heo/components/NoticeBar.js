d94c07a5bf34b9706e0295afcb16dacd93b219e5
youyouguo430 <139545589+youyouguo430@users.noreply.github.com>
2024-01-29 16:18:45 +0800

    NoticeBar.js

--- themes/heo/components/NoticeBar.js
+++ themes/heo/components/NoticeBar.js
@@ -20,7 +20,7 @@ export function NoticeBar() {
         <div className="max-w-[86rem] w-full mx-auto flex h-12 mb-4 px-5 font-bold">
             <div className="animate__animated animate__fadeIn animate__fast group cursor-pointer bg-[#f2a5a0] dark:bg-[#1e1e1e] dark:text-white hover:border-[#8c3837] dark:hover:border-yellow-600 border dark:border-gray-700  duration-200 hover:shadow-md transition-all rounded-xl w-full h-full flex items-center justify-between px-5">
                 <span className='whitespace-nowrap'>此刻</span>
-                <div className="w-full h-full hover:text-indigo-600 flex justify-center items-center">
+                <div className="w-full h-full hover:text-[#8c3837] flex justify-center items-center">
                     <Swipe items={notices} />
                 </div>
                 <div><ArrowRightCircle className={'w-5 h-5'} /></div>
